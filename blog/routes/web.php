e2eac891e986b905728d26b8ee120d8671297d76
Adrià Navarro Savall <s.adria.navarro@gmail.com>
2026-02-03 10:24:58 +0100

    Sesion 2 ejercicio 1 acabado

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -4,8 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio')
+;
 
 Route::get('/posts',function () {
-    return view('posts');
-});
+    return "Bienvenido a posts";
+})->name('posts_listado')
+;
+
+Route::get('/posts/{id}', function($id){
+    return "FIcha de posts numero: " . $id;
+})-> Where('id', "[0-9]+")
+->name('posts_ficha')
+;
